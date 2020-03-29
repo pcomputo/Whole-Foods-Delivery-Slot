6bca1f40a2a2b1f7d79e6d5ebe85e9942e735e65
Pooja <ahuja.pooja22@gmail.com>
2020-03-29 09:50:06 -0700

    Added additional checks for faster functionality

--- whole_foods_delivery_slot.py
+++ whole_foods_delivery_slot.py
@@ -6,9 +6,7 @@ import urllib
 
 from selenium import webdriver
 
-
 import json
-
 
 import sys
 import time
@@ -23,7 +21,7 @@ from PyQt4.QtWebKit import QWebPage
 '''
 
 
-def getAmazonPrice(productUrl):
+def getWFSlot(productUrl):
    headers = {
        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
    }
@@ -32,44 +30,48 @@ def getAmazonPrice(productUrl):
    driver.get(productUrl)           
    html = driver.page_source
    soup = bs4.BeautifulSoup(html)
-   time.sleep(50)   
-   while 100:
-   	driver.refresh()
-   	print("refreshed")
-   	html = driver.page_source
-   	soup = bs4.BeautifulSoup(html)
-   	time.sleep(10)
-   	pattern = 'No delivery windows available. New windows are released throughout the day.'
-   	if pattern == soup.find('h4', class_ ='a-alert-heading').text:
-   		mydivs = soup.find_all("div", class_ = "a-box a-alert a-alert-info ufss-slotselect-unavailable-alert")
-   		if mydivs:
-   			print("NO SLOTS!")
-   		else:
-   			print("SLOTS OPEN!")
-   	else:
-   		os.system('say "Slots for delivery opened!"')
-   	#print(mydivs)
-   	''''
-   	for foo in soup.find_all('div', attrs={'class': 'ufss-widget-grid'}):
+   time.sleep(40)
+   no_open_slots = True
 
-   		#print(foo)
-   		
-   		foo_descendants = foo.descendants
-   		for d in foo_descendants:
-   		#print(d)
-   		 #if d.name == 'div' and d.get('class', '') == ['a-row']:
-   		 for poo in soup.find_all('div', attrs={'class': 'a-row ufss-widget-grid-row'}):
-   		 	poo_descendants = poo.descendants
-   		 	print("poo",poo.descendants)
-   		 	for p in poo_descendants:
-   		 		print(p)
-   		 		if p.name == 'div' and p.get('class', '') == ['a-column a-span10 a-span-last']:
-   		 		 print("NO SLOTS")
-   		 		 print()
-   		 	#print("NO SLOTS!")
-   		 	#print(d.text)
-   		 	'''
+   while no_open_slots:
+      driver.refresh()
+      print("refreshed")
+      html = driver.page_source
+      soup = bs4.BeautifulSoup(html)
+      time.sleep(4)
 
-price = getAmazonPrice('https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1')
-print(price)
+      '''
+      mydivs = soup.find_all("div", class_ = "a-box a-alert a-alert-info ufss-slotselect-unavailable-alert")
+      if mydivs:
+         print("NO SLOTS!")
+      else:
+         print("SLOTS OPEN!")
+         os.system('say "Slots for delivery opened!"')
+         no_open_slots = False
+      '''
+      slot_pattern = 'Next available'
+      try:
+         next_slot_text = soup.find('h4', class_ ='ufss-slotgroup-heading-text a-text-normal').text
+         if slot_pattern in next_slot_text:
+            print('SLOTS OPEN!')
+            os.system('say "Slots for delivery opened!"')
+            no_open_slots = False
+            time.sleep(140)
+      except AttributeError:
+         print("No slots!")
+         continue
+
+      try:
+         no_slot_pattern = 'No delivery windows available. New windows are released throughout the day.'
+         if no_slot_pattern == soup.find('h4', class_ ='a-alert-heading').text:
+            print("NO SLOTS!")
+      except AttributeError: 
+            print('SLOTS OPEN!')
+            os.system('say "Slots for delivery opened!"')
+            no_open_slots = False
+
+
+
+getWFSlot('https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1')
+
 
