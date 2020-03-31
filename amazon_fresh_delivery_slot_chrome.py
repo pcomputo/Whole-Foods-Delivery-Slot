0579fc78552f3a7489cfe18152e007d2b8aefbd9
Pooja <ahuja.pooja22@gmail.com>
2020-03-30 20:15:32 -0700

    Amazon fresh delivery slot finder

---
+++ amazon_fresh_delivery_slot_chrome.py
@@ -0,0 +1,42 @@
+import bs4
+
+from selenium import webdriver
+
+import sys
+import time
+import os
+
+
+def getWFSlot(productUrl):
+   headers = {
+       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+   }
+
+   driver = webdriver.Chrome()
+   driver.get(productUrl)           
+   html = driver.page_source
+   soup = bs4.BeautifulSoup(html)
+   time.sleep(60)
+   no_open_slots = True
+
+   while no_open_slots:
+      driver.refresh()
+      print("refreshed")
+      html = driver.page_source
+      soup = bs4.BeautifulSoup(html)
+      time.sleep(2)
+
+      try:
+         open_slots = soup.find('div', class_ ='orderSlotExists').text()
+         if open_slots != "false":
+            print('SLOTS OPEN!')
+            os.system('say "Slots for delivery opened!"')
+            no_open_slots = False
+            time.sleep(1400)
+      except AttributeError:
+         continue
+
+
+getWFSlot('https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1')
+
+
