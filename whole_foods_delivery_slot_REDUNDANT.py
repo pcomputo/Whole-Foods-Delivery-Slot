5a197941e6ae8d046b549a112fcf9387f6b22677
Pooja <ahuja.pooja22@gmail.com>
2020-04-08 07:51:01 -0700

    1hr 2hr added

--- whole_foods_delivery_slot_REDUNDANT.py
+++ whole_foods_delivery_slot_REDUNDANT.py
@@ -16,7 +16,7 @@ def getWFSlot(productUrl):
    driver.get(productUrl)           
    html = driver.page_source
    soup = bs4.BeautifulSoup(html)
-   time.sleep(60)
+   time.sleep(80)
    no_open_slots = True
 
    while no_open_slots:
@@ -26,28 +26,16 @@ def getWFSlot(productUrl):
       soup = bs4.BeautifulSoup(html)
       time.sleep(4)
 
-      slot_patterns = ['Next available', '1-hour delivery windows', '2-hour delivery windows']
+      slot_pattern = 'Next available'
       try:
          next_slot_text = soup.find('h4', class_ ='ufss-slotgroup-heading-text a-text-normal').text
-         if any(next_slot_text in slot_pattern for slot_pattern in slot_patterns):
+         if slot_pattern in next_slot_text:
             print('SLOTS OPEN!')
             os.system('say "Slots for delivery opened!"')
             no_open_slots = False
             time.sleep(1400)
       except AttributeError:
-         pass
-
-      try:
-         slot_opened_text = "Not available"
-         all_dates = soup.findAll("div", {"class": "ufss-date-select-toggle-text-availability"})
-         for each_date in all_dates:
-            if slot_opened_text not in each_date.text:
-               print('SLOTS OPEN!')
-               os.system('say "Slots for delivery opened!"')
-               no_open_slots = False
-               time.sleep(1400)
-      except AttributeError:
-         pass
+         continue
 
       try:
          no_slot_pattern = 'No delivery windows available. New windows are released throughout the day.'
