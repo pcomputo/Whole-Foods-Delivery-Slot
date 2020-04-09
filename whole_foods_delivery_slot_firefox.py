acaa4e22457be2ea91564e3ffbe42e35da93a25c
jason123santa <51246585+jason123santa@users.noreply.github.com>
2020-04-09 15:38:47 -0400

    Update whole_foods_delivery_slot_firefox.py

--- whole_foods_delivery_slot_firefox.py
+++ whole_foods_delivery_slot_firefox.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 import sys
 import time
 import os
+import winsound
 import pyttsx3
 
 engine = pyttsx3.init() # object creation
@@ -14,7 +15,7 @@ def sayIt(textToSay):
    engine.runAndWait()
 
 def getWFSlot(productUrl):
-   driver = webdriver.Firefox()
+   driver = webdriver.Firefox(executable_path=r"C:\Users\jason\Downloads\geckodriver-v0.26.0-win64\geckodriver.exe")
    driver.get(productUrl)           
    html = driver.page_source
    soup = bs4.BeautifulSoup(html)
@@ -28,16 +29,28 @@ def getWFSlot(productUrl):
       soup = bs4.BeautifulSoup(html)
       time.sleep(4)
 
-      slot_pattern = 'Next available'
+      slot_patterns = ['Next available', '1-hour delivery windows', '2-hour delivery windows']
       try:
          next_slot_text = soup.find('h4', class_ ='ufss-slotgroup-heading-text a-text-normal').text
-         if slot_pattern in next_slot_text:
+         if any(next_slot_text in slot_pattern for slot_pattern in slot_patterns):
             print('SLOTS OPEN!')
             sayIt("Slots for delivery opened!")
             no_open_slots = False
             time.sleep(1400)
       except AttributeError:
-         continue
+         pass
+
+      try:
+         slot_opened_text = "Not available"
+         all_dates = soup.findAll("div", {"class": "ufss-date-select-toggle-text-availability"})
+         for each_date in all_dates:
+            if slot_opened_text not in each_date.text:
+               print('SLOTS OPEN!')
+               sayIt("Slots for delivery opened!")
+               no_open_slots = False
+               time.sleep(1400)
+      except AttributeError:
+         pass
 
       try:
          no_slot_pattern = 'No delivery windows available. New windows are released throughout the day.'
@@ -47,7 +60,6 @@ def getWFSlot(productUrl):
             print('SLOTS OPEN!')
             sayIt("Slots for delivery opened!")
             no_open_slots = False
-
 
 sayIt("Starting")
 getWFSlot('https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1')
