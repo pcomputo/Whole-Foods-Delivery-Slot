3344a80d172013cede1118066c49ec76da9dcc4f
Pooja <ahuja.pooja22@gmail.com>
2020-04-09 14:38:06 -0700

    additional checks

--- whole_foods_delivery_autobuy.py
+++ whole_foods_delivery_autobuy.py
@@ -19,7 +19,6 @@ def autoCheckout(driver):
    time.sleep(1)
    try:
       slot_select_button = driver.find_element_by_xpath('/html/body/div[5]/div[1]/div/div[2]/div/div/div/div/div[1]/div[4]/div[2]/div/div[3]/div/div/ul/li/span/span/div/div[2]/span/span/button')
-   
       slot_select_button.click()
       print("Clicked open slot")
    except NoSuchElementException:
@@ -41,14 +40,13 @@ def autoCheckout(driver):
       pass
 
    try:
-      time.sleep(4)
-
+      time.sleep(6)
       payment_select_continue = driver.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div[4]/div/form/div[3]/div[1]/div[2]/div/div/div/div[1]/span/span/input')
       payment_select_continue.click()
       print("Payment method selected")
 
 
-      time.sleep(4)
+      time.sleep(6)
       try:
          review_select_continue = driver.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/form/div/div/div/div[2]/div/div[1]/div/div[1]/div/span/span/input')
          review_select_continue.click()
@@ -63,7 +61,7 @@ def autoCheckout(driver):
    except NoSuchElementException:
       print("Found a slot but it got taken, run script again.")
       os.system('say "Found a slot but it got taken, run script again."')
-      time.sleep(1400)
+      #time.sleep(1400)
 
 def getWFSlot(productUrl):
    headers = {
@@ -71,9 +69,10 @@ def getWFSlot(productUrl):
    }
 
    driver = webdriver.Chrome()
-   driver.get(productUrl)           
+   driver.get(productUrl)
+   driver.maximize_window()         
    html = driver.page_source
-   soup = bs4.BeautifulSoup(html)
+   soup = bs4.BeautifulSoup(html, "html.parser")
    time.sleep(60)
    no_open_slots = True
 
@@ -88,7 +87,7 @@ def getWFSlot(productUrl):
       try:
          next_slot_text = soup.find('h4', class_ ='ufss-slotgroup-heading-text a-text-normal').text
          if any(next_slot_text in slot_pattern for slot_pattern in slot_patterns):
-            print('SLOTS OPEN 1!')
+            print('SLOTS OPEN!')
             os.system('say "Slots for delivery opened!"')
             no_open_slots = False
 
@@ -102,7 +101,7 @@ def getWFSlot(productUrl):
          all_dates = soup.findAll("div", {"class": "ufss-date-select-toggle-text-availability"})
          for each_date in all_dates:
             if slot_opened_text not in each_date.text:
-               print('SLOTS OPEN 2!')
+               print('SLOTS OPEN!')
                os.system('say "Slots for delivery opened!"')
                no_open_slots = False
                autoCheckout(driver)
@@ -115,7 +114,7 @@ def getWFSlot(productUrl):
          if no_slot_pattern == soup.find('h4', class_ ='a-alert-heading').text:
             print("NO SLOTS!")
       except AttributeError: 
-            print('SLOTS OPEN 3!')
+            print('SLOTS OPEN!')
             os.system('say "Slots for delivery opened!"')
             no_open_slots = False
 
